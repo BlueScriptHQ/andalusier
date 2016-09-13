fabc07ef31a51f37f320ea8754304df838123ecf
Karin1996 <karinkreeft8@hotmail.com>
2016-09-13 14:47:03 +0200

    Begonnen met DatePicker
    Nog niet af. Moeite met coderen

---
+++ tryouts/datepicker2.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="../css/prefixes.css" media="screen" charset="utf-8">
+  <link rel="stylesheet" href="../css/style.min.css" media="screen" charset="utf-8">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+</head>
+<body>
+  <style>
+    input[type=text] {
+      margin: 10px 5px 10px 10px;
+      float: left;
+      cursor: pointer;
+      height: 30px;
+      padding: 5px;
+    }
+    img {
+      width: 30px;
+      height: 30px;
+      margin-top: 10px;
+      float: left;
+      cursor: pointer;
+    }
+    #birthdate_container {
+      width: 400px;
+      float: left;
+    }
+    #datepicker_wrapper{
+      display: none;
+    }
+    #datepicker {
+      width: 250px;
+      margin: 10px;
+      border: 2px solid #990909;
+      background-color: #fff;
+      z-index: 9999;
+      float: left;
+    }
+
+    #datepicker .year-month {
+      width: 100%;
+      height: 50px;
+      background-color: #990909;
+      color: white;
+      float: left;
+      line-height: 50px;
+      text-align: center;
+      position: relative;
+    }
+    #datepicker .year-month img {
+      width: 20px;
+      height: 20px;
+    }
+    #datepicker .year-month img.left-arrow {
+      left: 10px;
+      top: 5px;
+      position: absolute;
+    }
+
+    #datepicker .year-month h2 {
+
+    }
+
+    #datepicker .year-month img.right-arrow {
+      top: 5px;
+      right: 10px;
+      position: absolute;
+    }
+
+    #datepicker .days {
+
+    }
+    #datepicker .day {
+      width: 28px;
+      height: 28px;
+      border: 1px solid #990909;
+      float: left;
+      text-align: center;
+      line-height: 28px;
+      cursor: pointer;
+      margin: 3.5px;
+    }
+    #datepicker .day:hover{
+      background-color: #990909;
+      color: white;
+      transition: 300ms;
+    }
+  </style>
+
+  <div id="birthdate_container">
+    <input type="text" id="datepicker_input" maxlength="10">
+    <img id="datepicker_caller" src="../img/other/datepicker.png" />
+    <div id="datepicker_wrapper">
+      <div id="datepicker" class="datepicker">
+        <div class="year-month">
+          <img class="left-arrow" src="../img/other/left-arrow.png" alt="" />
+          <h2>September 2016</h2>
+          <img class="right-arrow" src="../img/other/right-arrow.png" alt="" />
+        </div>
+        <div class="days"></div>
+      </div>
+    </div>
+  </div>
+
+  <script type="text/javascript">
+      $(document).ready(function(){
+
+        function pickDate(e){
+          var key = "which" in e ? e.which : e.keyCode;
+          if (key == 8 || key == 46) {}//end if not events
+          else{
+            var userInput = $(this).val();
+            if(userInput.length == 2 || userInput.length == 5){
+              $(this).val(userInput + "/");
+            }//end if userInput.length
+          }//end keyevents else
+        }//end function pickDate
+
+        $("#datepicker_caller").on("click", function(){
+          // = [{ 'month_name': 'Januari', 'amount_of_days': 31 }, { 'month_name': 'Februari', 'amount_of_days': 29 }, { 'month_name': 'Maart', 'amount_of_days': 31 }, { 'month_name': 'April', 'amount_of_days': 30 }, { 'month_name': 'Mei', 'amount_of_days': 31 }, { 'month_name': 'Juni', 'amount_of_days': 30 }, { 'month_name': 'Juli', 'amount_of_days': 31 }, { 'month_name': 'Augustus', 'amount_of_days': 31 }, { 'month_name': 'September', 'amount_of_days': 30 }, { 'month_name': 'Oktober', 'amount_of_days': 31 }, { 'month_name': 'November', 'amount_of_days': 30 }, { 'month_name': 'December', 'amount_of_days': 31 }];
+          $("#datepicker_wrapper").show();
+          //function isValidDate(s) {
+          //  var bits = s.split('/');
+          //  var d = new Date(bits[2] + '/' + bits[1] + '/' + bits[0]);
+          //  return !!(d && (d.getMonth() + 1) == bits[1] && d.getDate() == Number(bits[0]));
+          //}
+        });
+
+        $("#datepicker_input").on("keydown", pickDate);
+      })
+  </script>
+
+</body>
+</html>
