8c5feade805f2e83d13311192dd3e044aa7678dc
GerritLuimstra <gerrit.luimstra@ziggo.nl>
2016-09-14 17:33:01 +0200

    Bestands structuur geupdate

--- elements/resources.php
+++ elements/resources.php
@@ -1,27 +1,28 @@
 <meta charset="UTF-8">
 
+<!-- stylesheets -->
 <link rel="stylesheet" href="css/prefixes.css" media="screen" charset="utf-8">
 <link rel="stylesheet" href="css/style.min.css" media="screen" charset="utf-8">
 <link rel="stylesheet" href="css/popups_style.min.css" media="screen" charset="utf-8">
 
-
+<!-- vendor scripts -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-
 <script src="http://developers.bluescript.nl/libraries/requirements.min.js" charset="utf-8"></script>
 
-<script src="js/assets/config/variables.js" charset="utf-8"></script>
-<script src="js/assets/validators/input_class.js" charset="utf-8"></script>
+<!-- global requirements -->
+<script src="js/assets/var/variables.js" charset="utf-8"></script>
+<script src="js/assets/validator.js" charset="utf-8"></script>
+<script src="js/assets/functions.js" charset="utf-8"></script>
 
-<script src="js/layout/menu_structure.js" charset="utf-8"></script>
+<!-- layout requirements -->
+<script src="js/layout/side-menu.js" charset="utf-8"></script>
 
-<script src="js/functions.js" charset="utf-8"></script>
-
-
+<!-- AJAX handlers and events -->
 <script src="js/ajax/general.ajax.js" charset="utf-8"></script>
 <script src="js/controls/general.controls.js" charset="utf-8"></script>
 
-<!-- to be removed later -->
+<!-- Optional (temporary) stuff. This will be removed when the website goes live -->
 <script src="js/optional/comment_optional.js" charset="utf-8"></script>
 
 <!-- and eventually -->
-<script src="js/page_start.js" charset="utf-8"></script>
+<script src="js/start.js" charset="utf-8"></script>
