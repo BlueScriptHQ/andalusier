1b779ef6279de43d001fc7b55ea8206d91853700
Karin1996 <karinkreeft8@hotmail.com>
2016-09-16 23:59:07 +0200

    Verder bezig met datepicker

--- tryouts/datepicker2js.js
+++ tryouts/datepicker2js.js
@@ -1,14 +1,24 @@
 $(document).ready(function(){
 
   function pickDate(e){
-    var key = "which" in e ? e.which : e.keyCode;
-    if (key == 8 || key == 46) {}//end if not events
+    var userInput = $(this).val();
+    if ($.isNumeric(userInput.replace(/-/g, ""))) {
+      var key = "which" in e ? e.which : e.keyCode;
+        if (key === 8 || key === 46) {}//end if not events
+        else{
+          if(userInput.length === 2 || userInput.length === 5){
+            $(this).val(userInput + "-");
+          }//end if userInput.length
+          else if(userInput.length === 10){
+            alert(isValidDate(userInput));
+          }
+        }//end keyevents else
+    }
     else{
-      var userInput = $(this).val();
-      if(userInput.length == 2 || userInput.length == 5){
-        $(this).val(userInput + "/");
-      }//end if userInput.length
-    }//end keyevents else
+      var faultyUserInput = userInput.substring(0, userInput.length-1);
+      $(this).val(faultyUserInput);
+      alert(faultyUserInput);
+    }
   }//end function pickDate
 
   var date = new Date();
@@ -21,13 +31,12 @@ $(document).ready(function(){
     var monthName = months[curr].month_name;
     var days = months[curr].amount_of_days; //days which belong to current month(curr)
 
-    console.log(month);
     $(".days").empty();
     for(i=0; i < days; i++){
       $(".days").append("<div class='day'>"+ (i + 1) +"</div>");
     }
     $(".year-month h2").text(monthName + " " + year);
-    $("#datepicker_wrapper").show();
+    $("#datepicker_wrapper").toggle();
   }
 
   $("#datepicker_caller").on("click", calcDate);
@@ -51,7 +60,16 @@ $(document).ready(function(){
 
   $(".days").on("click", ".day", function(){
     var clickedDay = $(this).text();
-    var tempMonth = month.toString();
+    $(".day").css({
+      backgroundColor: "#fff",
+      color: "#000"
+    });
+    $(this).css({
+      backgroundColor: "#990909",
+      color: "#fff"
+    });
+    $("#datepicker_wrapper").hide();
+    var tempMonth = (month + 1).toString();
     if(clickedDay.length == 1){
       clickedDay= "0"+clickedDay;
     }
@@ -59,8 +77,14 @@ $(document).ready(function(){
       tempMonth= "0"+tempMonth;
     }
 
-    $("#datepicker_input").val(clickedDay +"/"+tempMonth+"/"+year);
+    $("#datepicker_input").val(clickedDay +"-"+tempMonth+"-"+year);
   });
 
-  $("#datepicker_input").on("keydown", pickDate);
+  $("#datepicker_input").on("keyup", pickDate);
+
+  function isValidDate(s) {
+    var bits = s.split('-');
+    var d = new Date(bits[2] + '/' + bits[1] + '/' + bits[0]);
+    return !!(d && (d.getMonth() + 1) == bits[1] && d.getDate() == Number(bits[0]));
+  }
 });
