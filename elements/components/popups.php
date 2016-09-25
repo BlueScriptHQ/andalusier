97ab0f1d0101ae012ee513403396f9990b226994
GerritLuimstra <gerrit.luimstra@ziggo.nl>
2016-09-25 14:54:59 +0200

    Updated advanced popups

--- elements/components/popups.php
+++ elements/components/popups.php
@@ -46,9 +46,41 @@
          <h2>Contactgegevens</h2>
       </div>
       <div class="popup-section-content">
+        <div class="row">
+           <p class="label">Straatnaam + Nummer</p>
+           <input type="text" name="accounts_street">
+           <input type="text" name="accounts_street_nr">
+        </div>
+        <div class="row">
+          <p class="label">Woonplaats + Postcode</p>
+          <input type="text" name="accounts_place">
+          <input type="text" name="accounts_zip">
+        </div>
+        <div class="row">
+          <p class="label">Telefoon(s)</p>
+          <input type="text" name="accounts_phone">
+          <input type="text" name="accounts_phone">
+        </div>
+        <div class="row">
+          <p class="label">Woonplaats + Postcode</p>
+          <input type="text" name="accounts_place">
+          <input type="text" name="accounts_zip">
+        </div>
+        <div class="row">
+          <p class="label">Woonplaats + Postcode</p>
+          <input type="text" name="accounts_place">
+          <input type="text" name="accounts_zip">
+        </div>
+        <div class="row">
+          <p class="label">Land van herkomst</p>
+          <select>
+            <option value="nederland">Nederland</option>
+            <option value="belgie">België</option>
+          </select>
+        </div>
          <div class="row">
             <p class="label">&nbsp;</p>
-            <input type="button" value="Volgende" class="openNext">
+            <input type="button" value="Volgende stap" class="openNext">
          </div>
       </div>
    </div>
@@ -58,7 +90,51 @@
          <h2>Verenigingsgerelateerd</h2>
       </div>
       <div class="popup-section-content">
-         <input type="text" value="">
+        <div class="row">
+          <p class="label">Lid type</p>
+          <select>
+            <option value="1">Lid</option>
+            <option value="2">Jeugdlid</option>
+            <option value="3">Gezinslid</option>
+            <option value="4">Lid Buitenland</option>
+          </select>
+        </div>
+        <div class="row">
+          <p class="label">Nieuwsbrief</p>
+          <div class="cop_check_wrapper">
+            <div class="cop_container cop_con_check">
+              <div class="cop_inner cop_in_check"></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <p class="label">Stalnaam</p>
+          <input type="text">
+        </div>
+        <div class="row">
+          <p class="label">Bankrekening</p>
+          <input type="text">
+        </div>
+        <div class="row">
+          <p class="label">Ontvangen via</p>
+          <div class="op_option_wrapper">
+            <div class="cop_container op_con_option">
+              <div class="cop_inner op_in_option"></div>
+            </div>
+            <p class="tag">E-mail</p>
+          </div>
+
+          <div class="op_option_wrapper">
+            <div class="cop_container op_con_option">
+              <div class="cop_inner op_in_option"></div>
+            </div>
+            <p class="tag">Post</p>
+          </div>
+        </div>
+        <div class="row">
+           <p class="label">&nbsp;</p>
+           <input type="button" value="Volgende stap" class="openNext">
+        </div>
       </div>
    </div>
    <div class="popup-section">
@@ -71,6 +147,10 @@
       </div>
    </div>
 </div>
+
+
 <!-- requirements -->
 <link rel="stylesheet" href="elements/components/css/popups.css" media="screen">
+<link rel="stylesheet" href="elements/components/css/check_radio.css" media="screen">
 <script src="elements/components/popups.js"></script>
+<script src="elements/components/check_radio.js" charset="utf-8"></script>
---
+++ tryouts/new_components/check_radio.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  // init
+  $(".op_con_option:eq(0)").children().css("display", "block").attr("on", true);
+
+  function cop_check(){
+    if($(this).children().attr("on") === "true") {
+      $(this).children().attr("on", false);
+    }
+    else {
+      $(this).children().attr("on", true);
+    }
+    $(this).children().toggle();
+  }
+
+
+  function cop_option(){
+    $(".op_con_option").children().css("display", "none").attr("on", false);
+    $(this).children().css("display", "block").attr("on", true);
+  }
+
+  //controls
+  $(".cop_con_check").on("click", cop_check);
+  $(".op_con_option").on("click", cop_option);
+
+});
