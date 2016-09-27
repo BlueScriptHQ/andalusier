68813344d41cc60341d34cdbbdaf20cdb6cdd899
maaikejolein <maaikejolein@gmail.com>
2016-09-27 15:30:35 +0200

    Kleine schoonheidsfoutjes veranderd
    De code door een beautifier gehaald.

--- tryouts/fixBackslash/index.php
+++ tryouts/fixBackslash/index.php
@@ -1,27 +1,14 @@
 <?php
-
-// hier worden de inputs gemaakt.
-
-$input = "//test/";
-$input2 = "/test";
-$input3 = "test/df/";
-$input4 = "t/e\s\/t";
-
-// fix dit.
-// hier is een functie genaamd stripChars. met de parameter $input
+// hier is een functie genaamd stripSlash met de parameter $input
 
 function stripSlash($input)
 {
 
-	// str_replace verander / -> "" in $input
+    // str_replace verander / -> "" in $input
 
-	$result = str_replace("/", "", $input);
-	$result = str_replace("\\", "", $result);
-	return $result;
+    $result = str_replace("/", "", $input);
+    $result = str_replace("\\", "", $result);
+    return $result;
 }
 
-echo stripSlash($input) . "<br />";
-echo stripSlash($input2) . "<br />";
-echo stripSlash($input3) . "<br />";
-echo stripSlash($input4) . "<br />";
 ?>
