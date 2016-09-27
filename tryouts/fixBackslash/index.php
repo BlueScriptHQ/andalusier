f93fbab47fec7b0de619785fb11f630db6f35be3
Gerruuuth <gerrit_kreeft@hotmail.com>
2016-09-27 15:26:39 +0200

    PHP code mooi gemaakt
    Beautifier

--- tryouts/fixBackslash/index.php
+++ tryouts/fixBackslash/index.php
@@ -1,22 +1,27 @@
 <?php
 
-  //hier worden de inputs gemaakt.
-  $input = "//test/";
-  $input2 = "/test";
-  $input3 = "test/df/";
-  $input4 = "t/e\s\/t";
+// hier worden de inputs gemaakt.
 
-  // fix dit.
-  //hier is een functie genaamd stripChars. met de parameter $input
-  function stripSlash($input){
-    //str_replace verander / -> "" in $input
-    $result = str_replace("/", "", $input);
-    $result = str_replace("\\", "", $result);
-    return $result;
-  }
+$input = "//test/";
+$input2 = "/test";
+$input3 = "test/df/";
+$input4 = "t/e\s\/t";
 
-  echo stripSlash($input) . "<br>";
-  echo stripSlash($input2). "<br>";
-  echo stripSlash($input3). "<br>";
-  echo stripSlash($input4). "<br>";
+// fix dit.
+// hier is een functie genaamd stripChars. met de parameter $input
+
+function stripSlash($input)
+{
+
+	// str_replace verander / -> "" in $input
+
+	$result = str_replace("/", "", $input);
+	$result = str_replace("\\", "", $result);
+	return $result;
+}
+
+echo stripSlash($input) . "<br />";
+echo stripSlash($input2) . "<br />";
+echo stripSlash($input3) . "<br />";
+echo stripSlash($input4) . "<br />";
 ?>
