86aa3eb9f3a492cf9a536c7435b053d743f66bdc
maaikejolein <maaikejolein@gmail.com>
2016-10-25 14:47:13 +0200

    Begin adverteerders pagina

---
+++ adverts.php
@@ -0,0 +1,89 @@
+<?php
+  session_start();
+  require "php/assets/verifications.php";
+?>
+<!DOCTYPE html>
+<html lang="nl">
+  <head>
+    <title>Andalusier | Adverteerders</title>
+
+    <?php
+      // Vendor scripts (externe libraries: jQuery etc)
+      // Deze moeten bovenaan
+      require "elements/vendors.php";
+    ?>
+    <!-- page required scripts -->
+    <script src="js/ajax/accounts.ajax.js" charset="utf-8"></script>
+    <script src="js/controls/accounts.controls.js" charset="utf-8"></script>
+
+    <?php
+      // Algemene resources
+      require "elements/resources.php";
+    ?>
+
+  </head>
+  <body>
+
+    <?php
+      require "elements/loader.php";
+      require "elements/components/popups.php";
+      require "optional/comments.php";
+    ?>
+
+    <div id="page-wrapper">
+      <script type="text/javascript">
+        // page config
+        $pageTitle = "Accounts";
+      </script>
+      <!-- header section -->
+      <?php
+        require_once "elements/header.php";
+      ?>
+
+      <!-- content section -->
+
+      <div id="content-wrapper">
+        <div id="side-menu">
+          <ul>
+              <!-- Hier komt het menu, ingeladen met AJAX -->
+          </ul>
+        </div>
+        <div id="content">
+
+          <div class="tableHead">
+
+            <div class="button-box">
+              <input type="button" class="btn" value="Adverteerder toevoegen">
+            </div>
+
+          </div>
+
+          <div id="members-table" class="">
+            <div class="box-content">
+              <div class="box-row">
+                <table>
+                    <tr class="accounts-tableHead">
+                      <th>#</th>
+                      <th>Naam</th>
+                      <th>Soort</th>
+                      <th>Status</th>
+                      <th></th>
+                    </tr>
+
+                    <tr>
+                      <td>4</td>
+                      <td>test</td>
+                      <td>ldpelpde</td>
+                      <td>ok</td>
+                      <td>open</td>
+                    </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
