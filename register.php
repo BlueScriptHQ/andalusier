29ada7d1ff03ae57c4ccc0c80a404c4a861bf051
Gerruuuth <gerrit_kreeft@hotmail.com>
2016-11-02 14:39:56 +0100

    Register pagina

--- register.php
+++ register.php
@@ -21,11 +21,62 @@
     <img src="img/other/loader.gif" alt="" />
   </div>-->
   <div id="wrapper">
-      <div id="register_form">
+      <div id="register-form">
         <div id="header">
           <h2>Vereniging van het spaanse paard</h2>
           <img src="img/header-section/brand.png">
         </div>
+        <div id="register-form-content">
+          <div id="contact-block">
+            <p>Contactgegevens</p>
+            <img src="elements/components/img/indicator-image-right.png">
+          </div>
+          <div id="contact-form">
+            <div class="row">
+               <p class="label">Naam</p>
+               <input type="text" name="register_name">
+            </div>
+            <div class="row">
+               <p class="label">Adres</p>
+               <input type="text" name="register_address">
+            </div>
+            <div class="row">
+               <p class="label">Woonplaats</p>
+               <input type="text" name="register_city">
+            </div>
+            <div class="row">
+               <p class="label">Telefoon of mobiel</p>
+               <input type="text" name="register_tel">
+            </div>
+            <div class="row">
+               <p class="label">Email</p>
+               <input type="text" name="register_email">
+            </div>
+            <div class="row">
+               <p class="label">Geboortedatum</p>
+               <input type="text" name="register_dateofbirth">
+            </div>
+          </div>
+          <div id="register-as-block">
+            <p>Lidsoort</p>
+            <img src="elements/components/img/indicator-image-right.png">
+          </div>
+
+        <div id="register-as-block-content">
+          <div class="row full-width">
+             <p class="label">Lidsoort kiezen:</p>
+             <fieldset>
+             <input type="radio" name="lid" value="Lid">Lid</input>
+             <br>
+             <input type="radio" name="lid" value="Jeugdlid">Jeugdlid</input>
+             <br>
+             <input type="radio" name="lid" value="Gezinslid">Gezinslid</input>
+             <br>
+             <input type="radio" name="lid" value="Lid-buitenland">Lid buitenland</input>
+           </fieldset>
+          </div>
+        </div>
+    </div>
     </div>
   </div>
 
